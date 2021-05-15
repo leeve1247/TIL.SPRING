414b24e4b9edec42687aebddcc149e3f4887f3c0
leeve1247 <multirkh@gmail.com>
2021-05-15 23:43:49 +0900

    DIP 를 위반한 설계

---
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        // given(주어졌을 때)
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when (이걸로 이런 실행을 했을 때)
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then(그에 대한 결과가?)
+        Assertions.assertThat(member).isEqualTo(findMember);
+//        member에 들어간 객체가 findMember와 동일할 경우 Tests passed가 뜹니다.
+    }
+}
