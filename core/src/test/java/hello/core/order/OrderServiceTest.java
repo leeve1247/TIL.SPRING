7fba5288c7646417204b01bec14fdf32f7600b85
leeve1247 <multirkh@gmail.com>
2021-05-16 01:28:33 +0900

    예시 변경

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -26,8 +26,8 @@ public class OrderServiceTest {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "ItemA", 10000);
-        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+        Order order = orderService.createOrder(memberId, "ItemA", 20000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(2000);
     }
 
 
