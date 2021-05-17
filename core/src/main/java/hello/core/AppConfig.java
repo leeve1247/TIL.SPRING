df7aeb76d2f0a9eed4a56cc3b3e49a407c3fd71f
leeve1247 <multirkh@gmail.com>
2021-05-17 23:20:32 +0900

    AppConfig의 SpringFrameWork 화

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,30 +1,37 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+// 설정정보, 구성정보를 담을 때 @Configuration 을 달아줍니다.
+@Configuration
 public class AppConfig {
 
-
+    //Bean 을 하면 Spring Container 에 등록이 된다.
+    @Bean
     public MemberService memberService(){
 //        구현체 생성을 대신 진행해 주는 것을 생성자 주입이라고 합니다.
        return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,17 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 스프링 컨테이너 객체를 생성하고, 해당 객체에서 AppConfig의 구현체를 불러와서 사용할 준비를 합니다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // appConfig.memberService로 불러오는게 아니고 위에것 처럼 불러오게 됨
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
 //        ctrl + alt + v 하면 좌측 변수명 선언이 알아서 됩니다.
 //        1L 인 이유는 long type이라, 그냥 1 넣으면 wrongtype error 발생
