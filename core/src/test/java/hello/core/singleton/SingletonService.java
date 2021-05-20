4efc16a4bf7e95e4fc360571fc2fe6af3b67ad01
leeve1247 <multirkh@gmail.com>
2021-05-20 22:14:38 +0900

    싱글톤 구조의 객체 생성 Test 케이스

---
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,21 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+    //static 이라고 하면 단 하나만 등록된다.
+    //다른 곳에서 new SingletonService라고 하면 참조 에러가 발생
+
+    //조회용
+    public static SingletonService getInstance(){
+        return instance;
+    }
+
+    //3. 생성자를 rivate로 선언해서 외부에서 new 키워드를 사용한 객체 생성을 못하게 만든다.
+    private SingletonService(){
+    }
+
+    public void logic(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+}
