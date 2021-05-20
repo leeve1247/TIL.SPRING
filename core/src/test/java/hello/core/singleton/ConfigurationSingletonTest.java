107b5d85160a9d94b63b2d70852e47252251163a
leeve1247 <multirkh@gmail.com>
2021-05-21 01:05:09 +0900

    CGLIB 설명

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -30,4 +30,20 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+    @Test
+    void configurationDeep(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean class type = " + bean.getClass());
+        //예상 : bean class = class hello.core.AppConfig
+        //결과 : bean class type = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$address
+        //순수한 클래스가 아니라 다른게 붙었다? 어떻게 된걸까
+        //스프링은 내가 만든 클래스를 CGLIB이라는 바이트코드 조작 라이브러리를 사용해서 다른 클래스를 만들고,
+        //그 다른 클래스를 대안 클래스로서 등록함
+        //이 대안 클래스가 싱글톤을 보장함(어떻게 생성하고 구성하는지는 매우 복잡함)
+        
+        //AppConfig$$EnhancerBySpringCGLIB 은 AppConfig의 자식클래스라서 AppConfig로 조회가 가능함
+    }
 }
+
