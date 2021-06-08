ba59f2a89bdf0c741885e11999b8ebff10c4495f
leeve1247 <multirkh@gmail.com>
2021-06-08 20:42:33 +0900

    오타 수정

--- core/src/test/java/hello/core/singleton/SingletonService.java
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -11,7 +11,7 @@ public class SingletonService {
         return instance;
     }
 
-    //3. 생성자를 rivate로 선언해서 외부에서 new 키워드를 사용한 객체 생성을 못하게 만든다.
+    //3. 생성자를 private로 선언해서 외부에서 new 키워드를 사용한 객체 생성을 못하게 만든다.
     private SingletonService(){
     }
 
